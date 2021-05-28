7256519d7d448380dd3140432bc63f63209eadc8
zackeman <46926086+zackeman@users.noreply.github.com>
2021-05-28 16:07:17 +0200

    rekond inlagd

--- projektarbete/public/rekond.php
+++ projektarbete/public/rekond.php
@@ -2,9 +2,73 @@
 include 'header.php';
 ?>
 
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="rekond.css">
+    <title>AutoDec</title>
+</head>
+<body>
+    <div class="container">
+    <header>
+        <div class="hero">
+            <h1>AutoDec Tvätt & Rekond</h1>
+            <p>Att tvätta bilen hos är bästa valet för dig. Vi har tvättprogram för alla behov,<br> oavsett om det gäller en snabbtvätt eller en premiumtvätt med extra allt. Vi har generösa öppettider för att du ska kunna tvätta när du har tid.</p>
+            <button>
+                Se mer
+            </button>
+        </div>
+        <div class="logo">
+            <img src="bilder/logo1.png" alt="">
+        </div>
+    </header>
+<!-- ----- -->
+    <section>
+        <h2 class="hand-written">
+            Hälsningar Mohammed & Issa & Andreas.
+        </h2>
+        <div class="main-image">
+            <img src="bilder/main-image1.jpg"alt="">
+        </div>
+        <div class="val">
+            <ul>
+                <li><table></table></li>
+                
+            </ul>
+        </div>
+
+    </section>
+<!-- ----- -->
+    <footer>
+        <div class="contakt-info">
+            <p>
+                På vår station kan du känna dig trygg med att vi använder de mest effektiva och minst skadliga produkterna som finns tillgängliga, våra tvättkemikalier är självklart Svanenmärkta. 
+
+            </p>
+            <p>+46706969025</p>
+        </div>
+        <div class="social">
+            <ul>
+                <li><img src="bilder/facebook.png" alt="" class="social-icon"></li>
+                <li><img src="bilder/twitter.png" alt="" class="social-icon"></li>
+                <li><img src="bilder/whatsapp.png" alt="" class="social-icon"></li>
+                <li><img src="bilder/instagram.png" alt="" class="social-icon"></li>
+            </ul>
+        </div>
+        <div class="website">
+            <a href="http://autodec.com">www.autodec.com</a>
+        </div>
+
+    </footer>
+
+    </div>
+</body>
+</html>
+
 <!-- Footer --->
 <?php
-include 'footer.php';
+//include 'footer.php';
 ?>
 
 </body>
